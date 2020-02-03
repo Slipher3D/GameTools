3398998d2e0ad9e53a446b3d713e43e47e976d31
Slipher3D <slipher3d@gmail.com>
2020-02-02 23:50:27 -0600

    uploaded 0.4.1
    made changes to the interface.

--- gtools_panel.py
+++ gtools_panel.py
@@ -28,12 +28,13 @@ class Gtools_PT_Panel(bpy.types.Panel):
         #UseNum property
         row = box.row(align = True)
         row = box.prop(props, 'usenums')
-        #StartNum property
-        row = box.row(align = True)
-        row = box.prop(props, 'startnum')
-        #StepNum property
-        row = box.row(align = True)
-        row = box.prop(props, 'step')
+        if props.usenums is True:
+            #StartNum property
+            row = box.row(align = True)
+            row = box.prop(props, 'startnum')
+            #StepNum property
+            row = box.row(align = True)
+            row = box.prop(props, 'step')
         #MOR Operator Button
         row = box.row()
         row = box.operator('object.morenamer', text = "Rename")
@@ -59,27 +60,30 @@ class Gtools_PT_Panel(bpy.types.Panel):
         #export filetype property
         row = box.row(align = True)
         row = box.prop(props, 'export_filetype')
-        #FBX Options label
-        row = box.row(align = True)
-        box.label(text = "FBX Options")
-        #bake bool, applymods bool
-        row = box.row(align = True)
-        row.prop(props, 'bakeanim')
-        row.prop(props, 'applymods')
-        #smoothing list
-        row = box.row(align = True)
-        row = row.prop(props, 'smoothing')
-        #OBJ Options label
-        row = box.row(align = True)
-        box.label(text = "OBJ Options")
-        #anim, smooth bool
-        row = box.row(align = True)
-        row.prop(props, 'anim')
-        row.prop(props, 'smoothingo')
-        #materials, triangulate bool
-        row = box.row(align = True)
-        row.prop(props, 'materials')
-        row.prop(props, 'triangulate')
+
+        if props.export_filetype is '0':
+            #FBX Options label
+            row = box.row(align = True)
+            box.label(text = "FBX Options")
+            #bake bool, applymods bool
+            row = box.row(align = True)
+            row.prop(props, 'bakeanim')
+            row.prop(props, 'applymods')
+            #smoothing list
+            row = box.row(align = True)
+            row = row.prop(props, 'smoothing')
+        elif props.export_filetype is '1':
+            #OBJ Options label
+            row = box.row(align = True)
+            box.label(text = "OBJ Options")
+            #anim, smooth bool
+            row = box.row(align = True)
+            row.prop(props, 'anim')
+            row.prop(props, 'smoothingo')
+            #materials, triangulate bool
+            row = box.row(align = True)
+            row.prop(props, 'materials')
+            row.prop(props, 'triangulate')
         #lcexporter operator
         row = box.row(align = True)
         row = box.operator('object.lcexporter', text = "Export")
